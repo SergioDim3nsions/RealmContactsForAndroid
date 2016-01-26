0da27e4d433c2095cbfd473b784a9ffb92626f62
Sergio Vasco <sergiovascoportillo@hotmail.com>
2016-01-26 16:03:52 +0100

    modify view and viewholder from recyclerview

--- app/src/main/java/sergio/vasco/realmforandroid/app/ui/recyclerview/viewholders/ContactViewHolder.java
+++ app/src/main/java/sergio/vasco/realmforandroid/app/ui/recyclerview/viewholders/ContactViewHolder.java
@@ -2,6 +2,8 @@ package sergio.vasco.realmforandroid.app.ui.recyclerview.viewholders;
 
 import android.content.Context;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import com.carlosdelachica.easyrecycleradapters.adapter.EasyViewHolder;
 import sergio.vasco.androidforexample.presentation.model.PresentationContact;
@@ -10,6 +12,8 @@ import sergio.vasco.realmforandroid.R;
 public class ContactViewHolder extends EasyViewHolder<PresentationContact> {
 
     private Context context;
+    @Bind(R.id.firstName) TextView firstName;
+    @Bind(R.id.lastName) TextView lastName;
 
     public ContactViewHolder(Context context, ViewGroup parent) {
         super(context, parent, R.layout.view_contact);
@@ -18,7 +22,8 @@ public class ContactViewHolder extends EasyViewHolder<PresentationContact> {
     }
 
     @Override public void bindTo(PresentationContact value) {
-
+        firstName.setText(value.getFirstName());
+        lastName.setText(value.getLastName());
     }
 
 }
